ad4dfc29b2f1d95e0f98ce614e035a212540c456
Anthony Bao <anthonybao1999@gmail.com>
2016-08-21 15:09:53 -0700

    d

--- app.js
+++ app.js
@@ -86,13 +86,13 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use((req, res, next) => {
-    if (req.path === '/api/upload') {
-        next();
-    } else {
-        lusca.csrf()(req, res, next);
-    }
-});
+// app.use((req, res, next) => {
+//     if (req.path === '/api/upload') {
+//         next();
+//     } else {
+//         lusca.csrf()(req, res, next);
+//     }
+// });
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
 app.use((req, res, next) => {
@@ -194,38 +194,6 @@ app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
     failureRedirect: '/login'
 }), (req, res) => {
     res.redirect(req.session.returnTo || '/');
-});
-
-/**
- * OAuth authorization routes. (API examples)
- */
-app.get('/auth/foursquare', passport.authorize('foursquare'));
-app.get('/auth/foursquare/callback', passport.authorize('foursquare', {
-    failureRedirect: '/api'
-}), (req, res) => {
-    res.redirect('/api/foursquare');
-});
-app.get('/auth/tumblr', passport.authorize('tumblr'));
-app.get('/auth/tumblr/callback', passport.authorize('tumblr', {
-    failureRedirect: '/api'
-}), (req, res) => {
-    res.redirect('/api/tumblr');
-});
-app.get('/auth/steam', passport.authorize('openid', {
-    state: 'SOME STATE'
-}));
-app.get('/auth/steam/callback', passport.authorize('openid', {
-    failureRedirect: '/login'
-}), (req, res) => {
-    res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/pinterest', passport.authorize('pinterest', {
-    scope: 'read_public write_public'
-}));
-app.get('/auth/pinterest/callback', passport.authorize('pinterest', {
-    failureRedirect: '/login'
-}), (req, res) => {
-    res.redirect('/api/pinterest');
 });
 
 /**
